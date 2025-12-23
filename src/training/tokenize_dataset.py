171e371295bac7398950de1feb37eddb83219118
Jose Cisneros <jcisneros@meicaperu.com>
2025-12-23 14:33:01 -0500

    Fix labels and change tokenize_function definition to accept tokenizer as argument. Also change MODEL_NAME for project consistency

--- src/training/tokenize_dataset.py
+++ src/training/tokenize_dataset.py
@@ -1,10 +1,10 @@
 from transformers import AutoTokenizer
 
-MODEL_NAME = "google/flan-t5-base"
+MODEL_NAME = "google/flan-t5-small"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-def tokenize_function(example):
+def tokenize_function(example, tokenizer):
     # Tokenize inpput (prompt)
     model_inputs = tokenizer(
         example["input_text"],
@@ -14,13 +14,12 @@ def tokenize_function(example):
     )
 
     # Tokenize output (label)
-    with tokenizer.as_target_tokenizer():
-        labels = tokenizer(
-            example["input_text"],
-            truncation=True,
-            padding="max_length",
-            max_length=8,
-        )
+    labels = tokenizer(
+        example["target_text"],
+        truncation=True,
+        padding="max_length",
+        max_length=8,
+    )
 
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
